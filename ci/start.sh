57e741bb21f3d03cf831bbc1e3ee9b762cb75f4a
Adam Gensler <adam.gensler@calculi.com>
2019-07-26 12:53:12 -0700

    GX-3833 alternate java installation

--- ci/start.sh
+++ ci/start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-apt-get -y install software-properties-common
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
-apt-get update
-apt -y install zulu-12
+
+# apt-get -y install software-properties-common
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
+# apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
+# apt-get update
+# apt -y install zulu-12
+
+export JAVA_HOME=${PKG_BASE_DIR}/jdk
+export PATH=${PKG_BASE_DIR}/jdk/bin:${PATH}
 
 cp ${PKG_BASE_DIR}/server/SpringBootServer*.war .
 java -jar SpringBootServer-1.0.1.war &
